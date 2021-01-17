bed36c7ea8d3132a9f8931cc15204ffec5e58797
victor mussel <victor.mussel@hotmail.com>
2021-01-17 14:14:37 -0300

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $perfil = Auth::user()->nivel;
+
+        // 'super-admin',
+        // 'admin',
+        // 'user',
+        // 'user_ti',
+        // 'user_infra',
+
+        // if($perfil == "super-admin"){
+            
+        //     return view('home');
+
+        // }elseif($perfil == "admin"){
+
+        //     return view('home');
+
+        // }elseif($perfil == "user"){
+        //     return view('home');
+
+        // }elseif($perfil == "user_ti"){
+        //     return view('home');
+
+        // }elseif($perfil == "user_infra"){
+        //     return view('home');
+
+        // }
+
+        // $user = Auth::user();
+        // dd($user);
+
+        return view('home');
+    }
+}
